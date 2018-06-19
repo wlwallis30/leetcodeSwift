5c1c052b351864c19d42b41d82681df4e0b3daf5
wlwallis30 <wlwallis30@gmail.com>
2018-06-19 14:15:54 -0700

    adding another two tests for two sum

--- Tests/TwoSumTests.swift
+++ Tests/TwoSumTests.swift
@@ -57,9 +57,61 @@ class TwoSumTests: XCTestCase {
         let nums = [-1, 0, 1, 2, -1, -4]
         let res = twoSum.threeSum_15(numbers: nums)
         let expected = [[-1, -1, 2], [-1, 0, 1]]
-        printQuestionInfo("three sum @15")
+        self.printQuestionInfo("three sum @15")
         self.debugPrint(res.description)
         self.debugPrint(expected.description)
         XCTAssertEqual(res, expected)
+    }
+
+    func testThreeSumClosest_16() {
+        var nums = [-1, 2, 1, -4]
+        var target = 1
+        var res = twoSum.threeSum_16(numbers: nums, target: target)
+        var expected = 2
+
+        self.printQuestionInfo("three sum closest @16")
+        self.debugPrint(expected.description)
+        self.debugPrint(res.description)
+        XCTAssertEqual(res, expected)
+        nums = [3,-2,-5,3,-4]
+        target = -1
+        expected = -2
+        res = twoSum.threeSum_16(numbers: nums, target: target)
+        self.debugPrint(expected)
+        self.debugPrint(res)
+    }
+
+    func testFourSum_18() {
+    var nums = [-1, 0, 1, 2, -2, 0]
+    var target = 0
+    var res = twoSum.fourSum_18(numbers: nums, target: target)
+    var expected = [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
+
+    self.printQuestionInfo("four sum @18")
+    self.debugPrint(expected.description)
+    self.debugPrint(res.description)
+    XCTAssertEqual(expected, res)
+
+    nums =  [0, 0, 0, 0]
+    target = 1
+    res = twoSum.fourSum_18(numbers: nums, target: target)
+    expected = []
+    XCTAssertEqual(expected, res)
+
+    nums = [-1,-5,-5,-3,2,5,0,4]
+    target = -7
+    expected = [[-5,-5,-1,4], [-5,-3,-1,2]]
+    res = twoSum.fourSum_18(numbers: nums, target: target)
+    XCTAssertEqual(expected, res)
+    }
+
+    func testTwoSum_167() {
+    let nums = [2, 7, 11, 15]
+    let target = 9
+    let res = twoSum.twoSumSorted_167(nums: nums, target: target)
+    let expected = [1, 2]
+
+    self.printQuestionInfo("two sum @167")
+    XCTAssertEqual(expected, res)
     }
 }
