1de6f652fd4bf2f9775f29946f96e4beb804e3b6
wlwallis30 <wlwallis30@gmail.com>
2018-06-19 12:24:15 -0700

    adding two tests

--- Tests/TwoSumTests.swift
+++ Tests/TwoSumTests.swift
@@ -8,38 +8,58 @@ import XCTest
 @testable import LeetcodeSwift
 
 class TwoSumTests: XCTestCase {
-    private var debug: Bool
-    private var twoSum: TwoSum
-    private var resultStr: String
-    private var expectedStr: String
-    private var expectedNotEqual: String
-    private  var expectedIntStr: String
+    private let debug: Bool = true
+    private let twoSum = TwoSum()
+    private let resultStr = "result is: "
+    private let expectedStr = "expected is: "
+    private let expectedNotEqual = "not equal, expected is: "
+    private let expectedIntStr = "not equal, expected integer is: %d"
 
     override func setUp() {
-        self.debug = true
-        self.twoSum = TwoSum()
-        self.resultStr = "result is: "
-        self.expectedStr = "expected is: "
-        self.expectedNotEqual = "not equal, expected is: "
-        self.expectedIntStr = "not equal, expected integer is: %d"
-
-        print("Init before each test...")
+        super.setUp()
+        print("Init before each test...\n")
     }
 
     override func tearDown() {
-        print("Done after each test")
+        print("Done after each test\n")
+        super.tearDown()
     }
 
-    private func debugPrint(message: String) {
+    private func printQuestionInfo(_ questionInfo: String) {
         if self.debug {
-            print(message)
+            print("-------Now Testing------>>", questionInfo, "\n")
+        }
+    }
+    private func debugPrint(_ message: String) {
+        if self.debug {
+            print(message, "\n")
         }
     }
 
-    private func debugPrint(number: Int) {
+    private func debugPrint(_ number: Int) {
         if self.debug {
-            print("Integer is: ", number)
+            print("Integer is: ", number, "\n")
         }
+    }
+
+    func testTwoSum_1() {
+        let nums = [2, 7, 11, 15]
+        let target = 9
+        let res = twoSum.twoSum_1(nums: nums, target: target)
+        let expected = [0, 1]
+        printQuestionInfo("two sum @1")
+        self.debugPrint(res.description)
+        self.debugPrint(expected.description)
+        XCTAssertEqual(res, expected)
+    }
+
+    func testThreeSum_15() {
+        let nums = [-1, 0, 1, 2, -1, -4]
+        let res = twoSum.threeSum_15(numbers: nums)
+        let expected = [[-1, -1, 2], [-1, 0, 1]]
+        printQuestionInfo("three sum @15")
+        self.debugPrint(res.description)
+        self.debugPrint(expected.description)
+        XCTAssertEqual(res, expected)
     }
 }
-
