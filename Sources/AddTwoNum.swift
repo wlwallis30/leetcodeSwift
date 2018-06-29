6fd03c1eb433be2067baa1b922f69a34c453e081
wlwallis30 <wlwallis30@gmail.com>
2018-06-29 13:08:21 -0700

    adding 43 and test

--- Sources/AddTwoNum.swift
+++ Sources/AddTwoNum.swift
@@ -30,7 +30,41 @@ extension AddTwoNumFunc {
         return res.next
     }
 
-    func addBinary(_ str1: String, _ str2: String) -> String {
+    func multiplyTwoStrs_43 (_ num1: String, _ num2: String) -> String {
+        let str1 = NSString(string: num1), str2 = NSString(string: num2)
+        let zero = "0" as NSString
+        let size1 = str1.length, size2 = str2.length
+        let maxReulstSize = size1 + size2
+        let startIdx = maxReulstSize - 2
+        var tmpRes = Array(repeating: 0, count: maxReulstSize)
+
+        for idx1 in 0..<size1 {
+            for idx2 in 0..<size2 {
+                let digit1 = str1.character(at: idx1) - zero.character(at: 0)
+                let digit2 = str2.character(at: idx2) - zero.character(at: 0)
+                tmpRes[startIdx - idx1 - idx2] += Int(digit1) * Int(digit2)
+            }
+        }
+
+        var carry = 0
+        for idx in 0..<maxReulstSize {
+            tmpRes[idx] += carry
+            carry = tmpRes[idx] / 10
+            tmpRes[idx] = tmpRes[idx] % 10
+        }
+
+        var idx = startIdx + 1
+        while tmpRes[idx] == 0 { idx -= 1 }
+        if idx < 0 { return "0" }
+        var res = ""
+        while idx >= 0 {
+            res += String(tmpRes[idx])
+            idx -= 1
+        }
+        return res
+    }
+
+    func addBinary_67(_ str1: String, _ str2: String) -> String {
         var res = ""
         let str1 = NSString(string: str1), str2 = NSString(string: str2)
         let zero = "0" as NSString
--- Tests/AddTwoNumTests.swift
+++ Tests/AddTwoNumTests.swift
@@ -10,10 +10,21 @@ import XCTest
 class AddTwoNumTests: LCBaseTest {
     private let addTwo = AddTwoNums()
 
+    func testAddTwo_2() {
+
+    }
+
+    func testMultiplyNums_43() {
+        let num1 = "2"
+        let num2 = "3"
+        let res = addTwo.multiplyTwoStrs_43(num1, num2)
+        XCTAssertEqual(res, "6")
+    }
+
     func testAddBinary_67() {
         let str1 = "1010"
         let str2 = "1011"
-        let res = addTwo.addBinary(str1, str2)
+        let res = addTwo.addBinary_67(str1, str2)
         let expected = "10101"
         self.debugPrint(res, self.printClosure)
         self.debugPrint(expected, self.printClosure)
