f6675205588ba27aada6aac4f1b78b346ba39a33
wlwallis30 <wlwallis30@gmail.com>
2018-07-08 23:34:28 -0700

    76's two versions, adding extension for String, but it is much slower than NSString version

--- Sources/SubStrNoRepeat.swift
+++ Sources/SubStrNoRepeat.swift
@@ -34,4 +34,85 @@ extension SubStrNoRepeatFunc {
 
         return res
     }
+
+    func minWindowSubStr_76(_ source: String, _ target: String) -> String {
+        var res = ""
+        if source.isEmpty || target.count > source.count { return res }
+
+        var targetCharMap = [Character: Int]()
+        var sourceCharMap = [Character: Int]()
+        for eachChar in target {
+            sourceCharMap[eachChar] = 0
+            if targetCharMap[eachChar] != nil {
+                targetCharMap[eachChar]! += 1
+            } else {
+                targetCharMap[eachChar] = 1
+            }
+        }
+        var left = 0, minResLen = source.count + 1, goalCount = 0
+        for right in 0..<source.count {
+            var curChar: Character = source[right]
+            if targetCharMap[curChar] != nil {
+                sourceCharMap[curChar]! += 1
+                if sourceCharMap[curChar]! <= targetCharMap [curChar]! { goalCount += 1 }
+            }
+
+            while(goalCount == target.count) {
+                curChar = source[left]
+                if right-left+1 < minResLen {
+                    minResLen = right - left + 1
+                    res = source[Range(left...right)]
+                }
+
+                if sourceCharMap[curChar] != nil {
+                    sourceCharMap[curChar]! -= 1
+                    if sourceCharMap[curChar]! < targetCharMap [curChar]! { goalCount -= 1 }
+                }
+                left += 1
+            }
+        }
+        return  res
+    }
+
+    func minWindowSubStr_76_1(_ source: String, _ target: String) -> String {
+        var res = ""
+        if source.isEmpty || target.count > source.count { return res }
+
+        let source = NSString(string: source)
+        let target = NSString(string: target)
+        var targetCharMap = [unichar: Int]()
+        var sourceCharMap = [unichar: Int]()
+        for idx in 0..<target.length {
+            let eachChar = target.character(at: idx)
+            sourceCharMap[eachChar] = 0
+            if targetCharMap[eachChar] != nil {
+                targetCharMap[eachChar]! += 1
+            } else {
+                targetCharMap[eachChar] = 1
+            }
+        }
+        var left = 0, minResLen = source.length + 1, goalCount = 0
+        for right in 0..<source.length {
+            var curChar = source.character(at: right)
+            if targetCharMap[curChar] != nil {
+                sourceCharMap[curChar]! += 1
+                if sourceCharMap[curChar]! <= targetCharMap [curChar]! { goalCount += 1 }
+            }
+
+            while(goalCount == target.length) {
+                curChar = source.character(at: left)
+                if right-left+1 < minResLen {
+                    minResLen = right - left + 1
+                    res = source.substring(with: NSRange(location: left, length: minResLen))
+                }
+
+                if sourceCharMap[curChar] != nil {
+                    sourceCharMap[curChar]! -= 1
+                    if sourceCharMap[curChar]! < targetCharMap [curChar]! { goalCount -= 1 }
+                }
+                left += 1
+            }
+        }
+        return  res
+    }
 }
--- Sources/Utils.swift
+++ Sources/Utils.swift
@@ -24,3 +24,17 @@ public class ListNode {
         self.next = nil
     }
 }
+
+extension String {
+    subscript (i: Int) -> Character {
+        return self[index(startIndex, offsetBy: i)]
+    }
+//    subscript (i: Int) -> String {
+//        return String(self[i] as Character)
+//    }
+    subscript (r: Range<Int>) -> String {
+        let start = index(startIndex, offsetBy: r.lowerBound)
+        let end = index(startIndex, offsetBy: r.upperBound)
+        return String(self[start..<end])
+    }
+}
